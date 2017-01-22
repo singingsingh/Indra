7c6526af77dc1c435e3c3a8b66acdb26db5222e6
Vikram Singh <vikramsingh.wanted@gmail.com>
2017-01-22 13:05:57 -0700

    Adding textures and sponza mesh model.

---
+++ Engine/Graphics/AssimpMesh.h
@@ -0,0 +1,56 @@
+#ifndef __ASSIMP_MODEL_H__
+#define __ASSIMP_MODEL_H__
+
+#include <Engine\Graphics\Texture.h>
+
+#include <d3d11.h>
+#include <d3dx10math.h>
+
+namespace Engine
+{
+	namespace Graphics
+	{
+		class AssimpMesh
+		{
+		public:
+			AssimpMesh();
+			~AssimpMesh();
+			bool initialize(char* modelFilename, const char* textureFilename);
+			void shutdown();
+			void render();
+			int getIndexCount();
+			ID3D11ShaderResourceView* getTexture();
+
+		private:
+			struct VertexType
+			{
+				D3DXVECTOR3 position;
+				D3DXVECTOR2 texture;
+				D3DXVECTOR3 normal;
+			};
+
+			struct AssimpData
+			{
+				float x, y, z;
+				float tu, tv;
+				float nx, ny, nz;
+			};
+
+			bool initializeBuffers();
+			void shutdownBuffers();
+			void renderBuffers();
+			bool loadModel(const char* modelFileName);
+			void releaseModel();
+			bool loadTexture(const char* textureFileName);
+			void releaseTexture();
+
+			ID3D11Buffer *_vertexBuffer, *_indexBuffer;
+			int _vertexCount, _indexCount;
+			Texture* _texture;
+			unsigned long* _index;
+			AssimpData* _assimpData;
+		};
+	}
+}
+
+#endif	//__ASSIMP_MODEL_H__
